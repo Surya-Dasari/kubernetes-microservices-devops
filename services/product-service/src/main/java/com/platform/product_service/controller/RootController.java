1549be959770c08fe44a653007dfae7e8f9cefa4
root <root@LAPTOP-GOCG20P5.localdomain>
2025-12-28 10:08:30 +0000

    Add root endpoint to product-service

---
+++ services/product-service/src/main/java/com/platform/product_service/controller/RootController.java
@@ -0,0 +1,13 @@
+package com.platform.product_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "product-service is up";
+    }
+}
