4f8f73ee8816711e0ec2cb2a06f1f09041de1f53
root <root@LAPTOP-GOCG20P5.localdomain>
2025-12-28 11:14:54 +0000

    all

---
+++ services/inventory-service/src/main/java/com/platform/inventoryservice/controller/InventoryController.java
@@ -0,0 +1,15 @@
+package com.platform.inventoryservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/inventory")
+public class InventoryController {
+
+    @GetMapping
+    public String inventory() {
+        return "Inventory Service OK";
+    }
+}
